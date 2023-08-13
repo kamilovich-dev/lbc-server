231eba4f64d41a89fad30cbb59c017a4098446ea
Daniyar <darkvoidder@gmail.com>
2023-08-13 15:08:27 +0300

    expanded Request interface with files field

--- src/declarations/@types-global.d.ts
+++ src/declarations/@types-global.d.ts
@@ -1,10 +1,12 @@
 import { UserDto } from 'dtos/user-dto'
+import { UploadedFile  } from 'express-fileupload'
 
 declare global {
 
     namespace Express {
         interface Request {
             user?: UserDto
+            files?: UploadedFile[] | undefined
         }
     }
 
